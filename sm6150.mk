9711255e0d095a8b4541ea949a54e6a4120adca0
AndreyDenikin1 <andrey.denikin8@gmail.com>
2022-07-09 10:30:03 +0300

    sm6150:Build MIUI Camera

--- sm6150.mk
+++ sm6150.mk
@@ -90,6 +90,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+#Build MIUI Camera
+$(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
+
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0.vendor \
