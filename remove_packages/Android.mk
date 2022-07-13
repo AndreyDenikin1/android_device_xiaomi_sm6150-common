a5d7b7e967692d9151a232ebd54ad9f0d42d83f3
AndreyDenikin1 <andrey.denikin8@gmail.com>
2022-07-13 07:47:33 +0300

    sm6150-common:delete Audio for vendor.prop

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -1,11 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := RemovePackages
+LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := YouTube Maps SafetyHubPrebuilt Drive PrebuiltGmail Camera2 CameraGo Eleven
+LOCAL_OVERRIDES_PACKAGES := \ 
+        YouTube Maps SafetyHubPrebuilt Drive PrebuiltGmail Camera2 CameraGo Eleven GrapheneCamera AudioFX
 LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
