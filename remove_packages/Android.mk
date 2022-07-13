7aa8b4ddf73308cdefa1a47ae320c336902dbc38
AndreyDenikin1 <andrey.denikin8@gmail.com>
2022-07-13 20:12:29 +0300

    sm6150-common:Fix RemovePkgs

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,8 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePkgs
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := \ 
-        YouTube Maps SafetyHubPrebuilt Drive PrebuiltGmail Camera2 CameraGo Eleven GrapheneCamera AudioFX
+LOCAL_OVERRIDES_PACKAGES := \
+  YouTube Maps SafetyHubPrebuilt Drive PrebuiltGmail Camera2 CameraGo Eleven GrapheneCamera AudioFX
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
